269cede6d356aa4ea8023fc4689b8db8fe9e2d36
lujaja <jarzcyber@gmail.com>
2023-12-20 11:20:15 +0300

    AirBnB_Clone update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- This script prepares a MySQL test server for the project hbnb clone
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'root'@'localhost';
