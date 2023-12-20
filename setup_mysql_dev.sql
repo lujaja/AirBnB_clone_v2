76ad674dc271bc00e172fc1763475684d3992199
lujaja <jarzcyber@gmail.com>
2023-12-20 13:22:27 +0300

    AirBnB_Clone update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- creates the MySQL server user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grants Permissions for user hbnb_test
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
