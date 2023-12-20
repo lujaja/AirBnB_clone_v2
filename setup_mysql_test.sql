296d7506daec65cb65cccc5284e90c0acc86c497
lujaja <jarzcyber@gmail.com>
2023-12-20 10:57:41 +0300

    AirBnB_Clone update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,5 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-use hbnb_test_db
 GRANT SELECT ON performance_schema.* TO 'root'@'localhost';
