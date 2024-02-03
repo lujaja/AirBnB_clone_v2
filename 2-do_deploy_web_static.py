200378faef83380a23100ae4a78782dff33ab2bc
lujaja <jarzcyber@gmail.com>
2024-02-03 14:11:14 +0300

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to web servers, using the function do_deploy
+    do_deploy
 """
-from fabric.api import run, put, env, local
+from fabric.api import run, put, env
 
 env.hosts = [
-        "18.207.141.60",
-        "52.91.154.167"
-    ]
+    "18.207.141.60",
+    "52.91.154.167"
+]
 
 def do_deploy(archive_path):
     """
@@ -16,36 +15,24 @@ def do_deploy(archive_path):
     """
     remote_path = "/tmp"
 
-    # upload the achive to the remote server
-    run("mkdir -p {}".format(remore_path))
+    # Upload the archive to the remote server
+    run("mkdir -p {}".format(remote_path))
     put(archive_path, remote_path)
 
-    # create directories and extract the archive
+    # Create directories and extract the archive
     release_path = "/data/web_static/releases"
-<<<<<<< HEAD
     release_folder = "web_static"
-    
-    run ("mkdir -p {}/{}".format(release_path, release_folder))
-    run("tar -xvzf {}/{} -C {}/{}".format(
-        remote_path,
-        archive_path.split("/")[-1],
-        release_path,
-        release_folder
-    ))
-=======
-    current_path = ""
-    # timestamp = run(date "+%Y%m%d%H%M%S")
-    run ("mkdir -p {}".format(release_path))
-    run("tar -xvzf /tmp/web_static_20240203004953.tgz {}/web_static".format(
-        release_path))
->>>>>>> e5a9770130a00f50f5b486ae0e1a9676f691bdc7
-    # cleanup
-    current_path = "/data/web_static/current"
-    run("rm -rf {}".format(current_path))i
+
+    run("mkdir -p {}/{}".format(release_path, release_folder))
+    run("tar -xvzf {}/{} -C {}/{}".format(remote_path, archive_path.split("/")[-1], release_path, release_folder))
+
+    # Cleanup
     run("rm {}/{}".format(remote_path, archive_path.split("/")[-1]))
-    # create symlink
-    run("ln -sf {}/{}/ {}".format(release_path, release_folder, current_path))
-    print("new version deployed!")
 
+    # Create symbolic link
+    current_path = "/data/web_static/current"
+    run("rm -rf {}".format(current_path))
+    run("ln -s {}/{}/ {}".format(release_path, release_folder, current_path))
 
+    print("New version deployed!")
 
