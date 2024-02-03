b9ffbc7adebdb34694a13c22aef6fe5c54bf2072
lujaja <jarzcyber@gmail.com>
2024-02-03 13:41:46 +0300

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,21 +17,27 @@ def do_deploy(archive_path):
     remote_path = "/tmp"
 
     # upload the achive to the remote server
-    run("mkdir -p /tmp")
+    run("mkdir -p {}".format(remore_path))
     put(archive_path, remote_path)
 
     # create directories and extract the archive
-    release_path = "/data/web_static/releases/"
-    current_path = ""
-    # timestamp = run(date "+%Y%m%d%H%M%S")
-    run ("mkdir -p {}".format(release_path))
-    run("tar -xvzf /tmp/web_static_20240203004953.tgz {}/web_static".fomart(
-        release_path))
+    release_path = "/data/web_static/releases"
+    release_folder = "web_static"
+    
+    run ("mkdir -p {}/{}".format(release_path, release_folder))
+    run("tar -xvzf {}/{} -C {}/{}".format(
+        remote_path,
+        archive_path.split("/")[-1],
+        release_path,
+        release_folder
+    ))
     # cleanup
-    run("rm /tmp/web_static_20240203004953.tgz")
-
-    run("mkdir -p /data/web_static/current")
-    run("ln -sf /data/web_static/releases/web_static /data/web_static/current")
+    current_path = "/data/web_static/current"
+    run("rm -rf {}".format(current_path))i
+    run("rm {}/{}".format(remote_path, archive_path.split("/")[-1]))
+    # create symlink
+    run("ln -sf {}/{}/ {}".format(release_path, release_folder, current_path))
+    print("new version deployed!")
 
 
 
