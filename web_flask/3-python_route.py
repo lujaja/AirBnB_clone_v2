4e6005c3ae6f23b95c90a5442ca05a44f47c279a
lujaja <jarzcyber@gmail.com>
2024-02-17 01:49:31 +0300

    web framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def c(text):
 @app.route("/python", defaults={"text": 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    return "python {}".format(html.escape(text).replace('_', ' '))
+    return "Python {}".format(html.escape(text).replace('_', ' '))
 
 
 if __name__ == "__main__":
